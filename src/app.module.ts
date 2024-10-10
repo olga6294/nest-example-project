9495794061c06b41cbcff65a8c5b85de73f8da49
olga6294 <opasieczynska@gmail.com>
2024-10-10 09:16:50 +0200

    add db

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { StarWarsModule } from './star-wars/star-wars.module';
 import { ConfigModule } from "@nestjs/config";
 import { configuration } from "./configuration";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [StarWarsModule, ConfigModule.forRoot({load: [configuration]})],
+  imports: [
+    StarWarsModule,
+    ConfigModule.forRoot({load: [configuration]}),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      url: process.env.POSTGRES_URL
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
